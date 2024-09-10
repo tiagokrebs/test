71e8e694fb6d283729e9937d47e9cb9b435945d0
Tiago Krebs <tiagokrebs@MacBook-Pro-de-Tiago.local>
2024-09-10 08:27:53 -0300

    order

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 import flask
-import logging
 from . import auth
 from . import orders
 from . import metrics
@@ -14,17 +13,17 @@ def create_app():
     def index():
         return flask.jsonify({'message': 'OK'})
     
+    # register blueprints if needed
     app.register_blueprint(auth.bp)
     app.register_blueprint(orders.bp)
 
     # extra
     metrics.setup_metrics(app)
+    logs.setup_logging(app)
 
     @app.after_request
     def after_request(response):
         app.logger.info(f"Request to {flask.request.path} completed with status {response.status_code}")
         return response
-
-    logs.setup_logging(app)
 
     return app
